fcaa5891b1df562b37cda6bdc3bb5d635ab54235
itsmaxymoo <maxoman10@gmail.com>
2020-11-12 10:21:36 -0500

    Webview, HTML setup

--- app/src/main/java/io/itsmaxymoo/sobrietytracker/MainActivity.kt
+++ app/src/main/java/io/itsmaxymoo/sobrietytracker/MainActivity.kt
@@ -2,10 +2,20 @@ package io.itsmaxymoo.sobrietytracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+
+		val w: WebView = findViewById(R.id.webViewMain)
+		w.settings.apply {
+			javaScriptEnabled = true
+			domStorageEnabled = true
+			allowFileAccess = true
+		}
+
+		w.loadUrl("file:///android_asset/index.html")
+	}
 }
