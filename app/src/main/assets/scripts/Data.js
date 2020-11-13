dad12b33059d6813e81c7d101e01bbf025a5a592
itsmaxymoo <maxoman10@gmail.com>
2020-11-13 11:08:33 -0500

    Tracker editor scripting and delete function

--- app/src/main/assets/scripts/Data.js
+++ app/src/main/assets/scripts/Data.js
@@ -1,17 +1,60 @@
-class Data{
-	static get trackerProfiles() {
-		return {
-			'alc': new TrackerProfile('Alcohol', 'las la-wine-bottle'),
-			'nic': new TrackerProfile('Nicotine', 'las la-smoking'),
-			'thc': new TrackerProfile('Weed', 'las la-cannabis'),
-			'porn': new TrackerProfile('Porn', 'las la-tired'),
-			'sugar': new TrackerProfile('Sugar', 'las la-cookie-bite'),
-			'fb': new TrackerProfile('Facebook', 'lab la-facebook-square'),
-			'reddit': new TrackerProfile('Reddit', 'lab la-reddit'),
-			'socialmedia': new TrackerProfile('Social Media', 'las la-hashtag'),
-			'other': new TrackerProfile('Other', 'las la-exclamation')
-		};
+class TrackerProfile {
+	constructor(name, icon){
+		this._name = name;
+		this._icon = icon;
 	}
+
+	get name(){
+		return this._name;
+	}
+
+	get icon(){
+		return this._icon;
+	}
+}
+
+class Tracker {
+	constructor(name = 'New Addiction', profile = 'other', date = new Date(), spentMoney = 0, spentTime = 0) {
+		this.name = name;
+		this.profile = profile;
+		this.date = date;
+		this.spentMoney = spentMoney;
+		this.spentTime = spentTime;
+	}
+
+	setDateHTML(date, time) {
+		date = new Date(date + " " + time);
+		now = new Date();
+
+		if(date instanceof Date && !isNaN(date) && date <= now) {
+			this.date = date;
+		}
+		else {
+			this.date = now;
+		}
+	}
+
+	getDateHTML() {
+		return [
+			this.date.getFullYear() + "-" + (this.date.getMonth() + 1) + "-" + this.date.getDate(),
+			this.date.getHours() + ":" + this.date.getMinutes()
+		];
+	}
+}
+
+class Data{
+	static trackerProfiles = {
+		'alc': new TrackerProfile('Alcohol', 'las la-wine-bottle'),
+		'nic': new TrackerProfile('Nicotine', 'las la-smoking'),
+		'thc': new TrackerProfile('Weed', 'las la-cannabis'),
+		'drugs': new TrackerProfile('Drugs', 'las la-capsules'),
+		'porn': new TrackerProfile('Porn', 'las la-tired'),
+		'sugar': new TrackerProfile('Sugar', 'las la-cookie-bite'),
+		'fb': new TrackerProfile('Facebook', 'lab la-facebook-square'),
+		'reddit': new TrackerProfile('Reddit', 'lab la-reddit'),
+		'socialmedia': new TrackerProfile('Social Media', 'las la-hashtag'),
+		'other': new TrackerProfile('Other', 'las la-exclamation')
+	};
 
 	static trackers = [];
 
@@ -27,48 +70,14 @@ class Data{
 	static write() {
 		localStorage.setItem('trackers', JSON.stringify(Data.trackers));
 	}
-}
 
-class Tracker {
-	constructor(name = 'Addiction', trackerProfile = 'other', date = Date.now(), spentMoney = 0, spentTime = 0) {
-		this.name = name;
-		this.trackerProfile = name;
-		this.date = date;
-		this.spentMoney = spentMoney;
-		this.spentTime = spentTime;
+	static createNewTracker() {
+		Data.trackers.push(new Tracker());
+
+		return Data.trackers.length - 1;
 	}
 
-	//TODO: Potential bug; HTML moths are 1-12 while JS months are 0-11
-	setDateHTML(date, time) {
-		date = Date.parse(date + " " + time);
-
-		if(date instanceof Date && !isNaN(date) && date <= Date.now()) {
-			this.date = date;
-		}
-		else {
-			this.date = Date.now();
-		}
-	}
-
-	getDateHTML() {
-		return [
-			this.date.getFullYear() + "-" + (this.date.getMonth() + 1) + "-" + this.date.getDate(),
-			this.date.getHours() + ":" + this.date.getMinutes()
-		];
-	}
-}
-
-class TrackerProfile {
-	constructor(name, icon){
-		this._name = name;
-		this._icon = icon;
-	}
-
-	get name(){
-		return this._name;
-	}
-
-	get icon(){
-		return this.icon;
+	static deleteTracker(id) {
+		Data.trackers.splice(id, 1);
 	}
 }
--- app/src/main/assets/scripts/main.js
+++ app/src/main/assets/scripts/main.js
@@ -1,25 +1,60 @@
-var elementEmptyStartMessage;
-var elementTrackerEditor;
-var elementTrackerEditorButtonsMain;
-var elementTrackerEditorButtonsDelete;
+// I wish JS had structs, but this will have to do for now.
+class elements {
+	static emptyStartMessage;
+	static trackerEditor;
+	static trackerEditorId;
+	static trackerEditorTitle;
+	static trackerEditorEditName;
+	static trackerEditorEditType;
+	static trackerEditorTypeIcon;
+	static trackerEditorEditDate;
+	static trackerEditorEditDateTime;
+	static trackerEditorEditSpentMoney
+	static trackerEditorEditSpentTime
+	static trackerEditorButtonsMain;
+	static trackedEditorButtonsDelete;
+}
 
+// Run once on app startup.
 document.addEventListener('contextmenu', event => event.preventDefault());
 window.onload = function() {
 	Data.load();
 
-	elementEmptyStartMessage = document.getElementById('empty-start-message');
-	elementTrackerEditor = document.getElementById('tracker-editor');
-	elementTrackerEditorButtonsMain = document.getElementById('tracker-editor-buttons-main');
-	elementTrackerEditorButtonsDelete = document.getElementById('tracker-editor-buttons-delete');
+	// Element references.
+	elements.emptyStartMessage = document.getElementById('empty-start-message');
+	elements.trackerEditor = document.getElementById('tracker-editor');
+	elements.trackerEditorId = document.getElementById('tracker-editor-id');
+	elements.trackerEditorTitle = document.getElementById('tracker-editor-title');
+	elements.trackerEditorEditName = document.getElementById('tracker-editor-edit-name');
+	elements.trackerEditorEditType = document.getElementById('tracker-editor-edit-type');
+	elements.trackerEditorTypeIcon = document.getElementById('tracker-editor-type-icon');
+	elements.trackerEditorEditDate = document.getElementById('tracker-editor-edit-date');
+	elements.trackerEditorEditDateTime = document.getElementById('tracker-editor-edit-date-time');
+	elements.trackerEditorEditSpentMoney = document.getElementById('tracker-editor-edit-spent-money');
+	elements.trackerEditorEditSpentTime = document.getElementById('tracker-editor-edit-spent-time');
+	elements.trackerEditorButtonsMain = document.getElementById('tracker-editor-buttons-main');
+	elements.trackerEditorButtonsDelete = document.getElementById('tracker-editor-buttons-delete');
 
-	hideElement(elementTrackerEditorButtonsDelete);
+	// Constructor tracker editor type dropdown.
+	for(key in Data.trackerProfiles){
+		var option = document.createElement('option');
+		option.text = Data.trackerProfiles[key].name;
+		option.value = key;
+		elements.trackerEditorEditType.add(option);
+	}
+	elements.trackerEditorTypeIcon.className = Data.trackerProfiles[Object.keys(Data.trackerProfiles)[0]].icon;
+
+	// Hide elements to not be shown by default.
+	hideElement(elements.trackerEditorButtonsDelete);
+	hideElement(elements.trackerEditor);
 
 	render();
 }
 
+// Runs multiple times.
 function render(){
 	if(Data.trackers.length > 0){
-		hideElement(elementEmptyStartMessage);
+		hideElement(elements.emptyStartMessage);
 	}
 }
 
@@ -29,4 +64,64 @@ function hideElement(e){
 
 function showElement(e){
 	e.style.display = 'block';
+}
+
+// GUI Functions.
+class GUI {
+	static createNewTracker() {
+		var trackerId = Data.createNewTracker();
+		elements.trackerEditorId.value = trackerId;
+		var tracker = Data.trackers[trackerId];
+		showElement(elements.trackerEditor);
+
+		elements.trackerEditorTitle.innerHTML = tracker.name;
+		elements.trackerEditorEditName.value = tracker.name;
+		elements.trackerEditorEditType.value = tracker.profile;
+		elements.trackerEditorEditDate.value = tracker.getDateHTML()[0];
+		elements.trackerEditorEditDateTime.value = tracker.getDateHTML()[1];
+		elements.trackerEditorEditSpentMoney.value = tracker.spentMoney;
+		elements.trackerEditorEditSpentTime.value = tracker.spentTime;
+
+		GUI.trackerEditorTypeIconUpdate();
+	}
+
+	static trackerEditorNameUpdate() {
+		elements.trackerEditorTitle.innerHTML = elements.trackerEditorEditName.value;
+	}
+
+	static trackerEditorTypeIconUpdate() {
+		elements.trackerEditorTypeIcon.className = Data.trackerProfiles[elements.trackerEditorEditType.value].icon;
+	}
+
+	static trackerEditorSetDisabled(disabled) {
+		elements.trackerEditorEditName.disabled = disabled;
+		elements.trackerEditorEditType.disabled = disabled;
+		elements.trackerEditorEditDate.disabled = disabled;
+		elements.trackerEditorEditDateTime.disabled = disabled;
+		elements.trackerEditorEditSpentMoney.disabled = disabled;
+		elements.trackerEditorEditSpentTime.disabled = disabled;
+	}
+
+	static trackerEditorDelete() {
+		GUI.trackerEditorSetDisabled(true);
+
+		hideElement(elements.trackerEditorButtonsMain);
+		showElement(elements.trackerEditorButtonsDelete);
+	}
+
+	static trackerEditorDeleteCancel() {
+		showElement(elements.trackerEditorButtonsMain);
+		hideElement(elements.trackerEditorButtonsDelete);
+
+		GUI.trackerEditorSetDisabled(false);
+	}
+
+	static trackerEditorDeleteConfirm() {
+		Data.deleteTracker(elements.trackerEditorId.value);
+		hideElement(elements.trackerEditor);
+		showElement(elements.trackerEditorButtonsMain);
+		hideElement(elements.trackerEditorButtonsDelete);
+
+		GUI.trackerEditorSetDisabled(false);
+	}
 }
